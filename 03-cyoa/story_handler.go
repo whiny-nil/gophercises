40d7add86b1f70cbbaec4aa0e6c524e978b492f7
Marc Kelsey <marc.kelsey@textnow.com>
2024-01-24 15:00:46 -0500

    Add templates, wrap stuff up

--- 03-cyoa/story_handler.go
+++ 03-cyoa/story_handler.go
@@ -1,9 +1,9 @@
 package main
 
 import (
-	"fmt"
-	"log"
+	"html/template"
 	"net/http"
+	"os"
 	"strings"
 )
 
@@ -16,10 +16,20 @@ func (bh StoryHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if key == "" {
 		key = "intro"
 	}
-	title := bh.storyJson[key].Title
-	log.Printf("%s", title)
 
-	fmt.Fprint(w, title)
+	rawTemplate, err := os.ReadFile("templates/story.tmpl.html")
+	if err != nil {
+		http.Error(w, "Error loading template", http.StatusInternalServerError)
+		return
+	}
+
+	tmpl, err := template.New("story").Parse(string(rawTemplate))
+	if err != nil {
+		http.Error(w, "Error parsing template", http.StatusInternalServerError)
+		return
+	}
+
+	err = tmpl.Execute(w, bh.storyJson[key])
 }
 
 func newStoryHandler(storyJson Story) *StoryHandler {
